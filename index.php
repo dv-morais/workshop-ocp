f93c0768279f2b3777fd22ffd3491fc834ba0151
dv-morais <57958197+dv-morais@users.noreply.github.com>
2019-11-19 16:20:29 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 echo "<h1>Openshift Workshop v3.0</h1> ";
 echo "<br><hr>";
 echo "<h2>Cidades cadastradas no Banco de Dados:</h2>";
-$conn = new mysqli("mysql", "redhat", "redhat@123", "workshop");
+//$conn = new mysqli("mysql", "redhat", "redhat@123", "workshop");
+$conn=  new PDO('mysql:host=mysql;dbname=workshop', 'redhat', 'redhat@123');
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
