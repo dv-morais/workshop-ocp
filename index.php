d4ed6d0b1a740e13f6b7defbf7560a5845fc81e7
dv-morais <57958197+dv-morais@users.noreply.github.com>
2019-11-19 15:25:07 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
-echo "<h1>Openshift Workshop v1.0</h1> ";
-echo $_SERVER['SERVER_ADDR'];
+echo "<h1>Openshift Workshop v1.0</h1> ";               
+  echo "<h1>Openshift Workshop v1.0</h1> ";                    
+  $host= gethostname();
+  $ip = gethostbyname($host);                    
+  echo $ip;
 ?>
