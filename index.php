69111b354c4d5208f19a8c42fc394de5041823a5
dv-morais <57958197+dv-morais@users.noreply.github.com>
2019-11-19 15:44:22 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
 echo "<h1>Openshift Workshop v1.0</h1> ";               
-  echo "<h1>Openshift Workshop v1.0</h1> ";                    
+  echo "<h1>Openshift Workshop v2.0</h1> ";                    
   $host= gethostname();
   $ip = gethostbyname($host);                    
   echo $ip;
-Vamo ver se é isso mesmo
 
 ?>
